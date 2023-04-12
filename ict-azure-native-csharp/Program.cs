b57eb8e5ea4e771d0fe9c3f80a02f0768b05a592
Sam Cogan <sam.cogan@willistowerswatson.com>
2023-04-12 11:20:58 +0100

    add template

---
+++ ict-azure-native-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    var config = new Config("samcogan");
+
+});
